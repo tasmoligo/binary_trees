a46eaeb2ae99512932f0ee213fb871e239b08072
Tasie Ibiam <tasieibiam28@gmail.com>
2023-03-30 07:36:18 -0700

    leaf and root

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -2,6 +2,7 @@
 
 /**
   * binary_tree_is_leaf - checks if a node is a leaf
+  * @node:  pointer to the node to check
   * Return: 1 if node is a leaf, otherwise 0
   */
 int binary_tree_is_leaf(const binary_tree_t *node)
--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -2,6 +2,7 @@
 
 /**
   * binary_tree_is_root - checks if a node is a leaf
+  * @node: pointer to the node to check
   * Return: 1 if node is a leaf, otherwise 0
   */
 int binary_tree_is_root(const binary_tree_t *node)
