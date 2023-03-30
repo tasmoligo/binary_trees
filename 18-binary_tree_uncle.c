69ef2707b2ffffd6253afe56b669d70dcaaf0777
Tasie Ibiam <tasieibiam28@gmail.com>
2023-03-30 07:56:32 -0700

    uncle in a tree

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -21,7 +21,8 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 		if (node->parent->parent->left == node->parent)
 		{
 			return (node->parent->parent->right);
-		}if (node->parent->parent->right == node->parent)
+		}
+		if (node->parent->parent->right == node->parent)
 		{
 			return (node->parent->parent->left);
 		}
